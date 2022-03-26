b1f2810d6a55babf0d4389b57dc55faa164ea6a7
taylorvercillo <taylornvcasey@gmail.com>
2022-03-26 13:54:43 -0600

    fixing post

--- src/components/CreateGod.js
+++ src/components/CreateGod.js
@@ -2,51 +2,38 @@ import React, { useState } from "react"
 
 function CreateGod({ handleAddGod }){
 
-    const [formData, setFormData] = useState({
-        name: "",
-        power: "",
-        romanname: "", 
-        image: ""
-    })
+    const [name, setName] = useState("")
+    const [power, setPower] = useState("")
 
-    const handleChange = (e) => {
-        setFormData({...formData, [e.target.name]: e.target.value})
-
-      }
-      
     
       function handleSubmit(e){
         e.preventDefault();
-        const updatedFormData = {...formData}
+        const godData ={
+          name: name, 
+          power: power,
+        }
         fetch("http://localhost:3004/gods", {
           method: "POST", 
           headers: {
             "Content-Type": "application/json", 
           }, 
-          body: JSON.stringify(updatedFormData), 
+          body: JSON.stringify(godData), 
         })
-          .then(res => res.json())
-          .then(data => console.log(data))
+          .then((res) => res.json())
+          .then((newGod) => console.log(newGod))
     
           setFormData({
             name:"",
-            power:"",
-            romanname:"",
-            image:""
+            power:""
           })
       }
-
-
-
 
     return (
         <div>
             <h1>Create A New God</h1>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" name="name" placeholder="God Name" value={formData.name} onChange={handleChange} />
-                    <input type="text" name="power" placeholder="Power" value={formData.power} onChange={handleChange} />
-                    <input type="text" name="romanname" placeholder="Roman Name" value={formData.romanname} onChange={handleChange} />
-                    <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleChange} />
+                    <input type="text" name="name" placeholder="God Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input type="text" name="power" placeholder="Power" value={power} onChange={(e) => setPower(e.target.value)} />
                     <button type="submit">Add God</button>
                 </form>
         </div>
